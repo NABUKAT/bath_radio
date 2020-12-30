2736ff705617b9a9695e5973feb0e077183aae52
Takuya Ban <mekakujira@infoseek.jp>
2020-12-30 21:32:27 +0900

    [fix] 微修正

--- roles/raspizero/files/setting.sh
+++ roles/raspizero/files/setting.sh
@@ -11,9 +11,10 @@ stationlist=`cat /opt/radio_core/stationlist.json`
 
 # bluetoothスピーカーのアドレスを設定
 # 現在値を表示し変更するかしないかを選択
-echo 現在設定されているBDアドレス
+echo ""
+echo "■現在設定されているBDアドレス"
 echo $device
-echo -n "変更しますか？(Y/N): "
+echo -n "→変更しますか？(Y/N): "
 # 変更する場合は入力受付
 read res
 if [ $res = "Y" ]; then
@@ -23,26 +24,27 @@ fi
 
 # 音量設定
 # 現在値を表示し変更するかしないかを選択
-echo 現在の音量設定
+echo ""
+echo "現在の音量設定"
 echo $vol
-echo -n "変更しますか？(Y/N): "
+echo -n "→変更しますか？(Y/N): "
 # 変更する場合は入力受付
 read res
 if [ $res = "Y" ]; then
-  echo -n "音量を入力してください: "
+  echo -n "音量を入力してください(0〜100): "
   read vol
 fi
 
 # 地域設定
 # 現在値を表示し変更するかしないかを選択
-echo 現在の地域設定
+echo ""
+echo "■現在の地域設定"
 echo $region
-echo -n "変更しますか？(Y/N): "
+echo -n "→変更しますか？(Y/N): "
 # 変更する場合は入力受付
 read res
 if [ $res = "Y" ]; then
-  echo ■地域リスト
-  echo ---------
+  echo "【地域リスト】"
   echo $stationlist | jq -r 'keys[]'
   echo ---------
   echo -n "リストから地域を選択してください: "
